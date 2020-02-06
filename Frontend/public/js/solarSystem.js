95f9efa920c7d74b39d9321cc3d1f19864f1166c
Alex Fisher <alfi6216@colorado.edu>
2020-02-05 19:31:32 -0700

    Set up toggling mechanism for toggling various planets to view. Also shifted the code to be more scalable and easier to add new objects and such.

--- Frontend/public/js/solarSystem.js
+++ Frontend/public/js/solarSystem.js
@@ -3,97 +3,36 @@ const viz = new Spacekit.Simulation(document.getElementById('main-container'), {
   basePath: 'https://typpo.github.io/spacekit/src',
 });
 
-viz.createStars();
-
-const sun = viz.createSphere('sun', {
-	//labelText: 'Sun', 
-	textureUrl: '/js/textures/2k_sun.jpg',
-	position: [0,0,0],
-	radius: 0.05
-});
-
-const mercury = viz.createSphere('mercury', {
-	labelText: 'Mercury',
-	textureUrl: '/js/textures/2k_mercury.jpg',
-	theme: {
-		//color: 0x913cee,
-	},
-	ephem: Spacekit.EphemPresets.MERCURY,
-	radius: 0.03
-});
-
-
-const venus = viz.createSphere('venus', {
-	labelText: "Venus",
-	textureUrl: '/js/textures/2k_venus_surface.jpg',
-	theme:{
-		//color: 0x913cee,
-	},
-	ephem: Spacekit.EphemPresets.VENUS,
-	radius: 0.035
-});
-
-const earth = viz.createSphere('earth', {
-	labelText: "Earth",
-	textureUrl: '/js/textures/2k_earth_daymap.jpg',
-	theme: {
-		//color: 0x913cee,
-	},
-	ephem: Spacekit.EphemPresets.EARTH,
-	radius: 0.035
-});
-
-const mars = viz.createSphere('mars', {
-	labelText: "Mars",
-	textureUrl: '/js/textures/2k_mars.jpg',
-	theme:{
-		//color: 0x913cee,
-	},
-	ephem: Spacekit.EphemPresets.MARS,
-	radius: 0.03
-});
-
-const jupiter = viz.createSphere('jupiter', {
-	labelText: "Jupiter",
-	textureUrl: '/js/textures/jupiter2_4k.jpg',
-	theme:{
-		//color: 0x913cee,
-	},
-	ephem: Spacekit.EphemPresets.JUPITER,
-	radius: 0.045
-});
-
-const saturn = viz.createSphere('saturn', {
-	labelText: "Saturn",
-	textureUrl: '/js/textures/2k_saturn.jpg',
-	theme:{
-		//color: 0x913cee,
-	},
-	ephem: Spacekit.EphemPresets.SATURN,
-	radius: 0.041
-});
-
-const uranus = viz.createSphere('uranus', {
-	labelText: "Uranus",
-	textureUrl: '/js/textures/2k_uranus.jpg',
-	theme:{
-		//color: 0x913cee,
-	},
-	ephem: Spacekit.EphemPresets.URANUS,
-	radius: 0.039
-});
-
-const neptune = viz.createSphere('neptune', {
-	labelText: 'Neptune',
-	textureUrl: '/js/textures/2k_neptune.jpg',
-	theme:{
-		//color: 0x913cee,
-	},
-	ephem: Spacekit.EphemPresets.NEPTUNE,
-	radius: 0.039
-});
+var expanded = false;
+var visualizer_list = {
+	"Sun" : viz.createSphere('sun', {textureUrl: '/js/textures/2k_sun.jpg', position: [0,0,0],radius: 0.05}), 
+	"Mercury" : viz.createSphere('mercury', {labelText: 'Mercury', textureUrl: '/js/textures/2k_mercury.jpg', theme: {/*color: 0x913cee,*/}, ephem: Spacekit.EphemPresets.MERCURY, radius: 0.03}), 
+	"Venus" : viz.createSphere('venus', {labelText: "Venus" , textureUrl: '/js/textures/2k_venus_surface.jpg', theme:{/*color: 0x913cee,*/}, ephem: Spacekit.EphemPresets.VENUS, radius: 0.035}), 
+	"Earth" : viz.createSphere('earth', {labelText: "Earth", textureUrl: '/js/textures/2k_earth_daymap.jpg', theme: {/*color: 0x913cee,*/}, ephem: Spacekit.EphemPresets.EARTH, radius: 0.035}), 
+	"Mars" : viz.createSphere('mars', {labelText: "Mars", textureUrl: '/js/textures/2k_mars.jpg', theme:{/*color: 0x913cee,*/}, ephem: Spacekit.EphemPresets.MARS, radius: 0.03}), 
+	"Jupiter" : viz.createSphere('jupiter', {labelText: "Jupiter", textureUrl: '/js/textures/jupiter2_4k.jpg', theme:{/*color: 0x913cee,*/}, ephem: Spacekit.EphemPresets.JUPITER, radius: 0.045}), 
+	"Saturn" : viz.createSphere('saturn', {labelText: "Saturn", textureUrl: '/js/textures/2k_saturn.jpg', theme:{/*color: 0x913cee,*/}, ephem: Spacekit.EphemPresets.SATURN, radius: 0.041}), 
+	"Neptune" : viz.createSphere('neptune', {labelText: 'Neptune', textureUrl: '/js/textures/2k_neptune.jpg', theme:{/*color: 0x913cee,*/}, ephem: Spacekit.EphemPresets.NEPTUNE, radius: 0.039}), 
+	"Uranus" : viz.createSphere('uranus', {labelText: "Uranus", textureUrl: '/js/textures/2k_uranus.jpg', theme:{/*color: 0x913cee,*/}, ephem: Spacekit.EphemPresets.URANUS, radius: 0.039})
+};
 
 viz.setCameraDrift(true);
+
+viz.createStars();
+
+var checkboxes = document.getElementById("checkboxes");
+for(let i of Object.keys(visualizer_list)){
+	appendCheckboxElement(checkboxes , i);
+	let checkbox_element = i.concat("-checkbox");
+	document.getElementById(checkbox_element).addEventListener("click" , function(){
+		let checked = document.getElementById(checkbox_element).checked;
+		if(!checked){
+			viz.removeObject(visualizer_list[i]);
+		} else {
+			viz.addObject(visualizer_list[i]);
+		}
+	});
+}
 
 document.addEventListener('mousedown', onDocumentMouseDown, false );
 
@@ -108,40 +47,35 @@ document.getElementById("stop-button").addEventListener("click", function() {
 document.getElementById("submit-button").addEventListener("click", function(){
 	let planetZoomChoice = document.getElementById("zoom-dropdown");
 	let choiceStr = planetZoomChoice.options[planetZoomChoice.selectedIndex].value;
-	console.log(choiceStr);
-	//Camera cam = viz.getViewer();
-	switch(choiceStr){
-		case "Sun":
-			console.log(choiceStr);
-			viz.getViewer().followObject(sun, [-0.75, -0.75, 0.5]);
-			break;
-		case "Mercury":
-			viz.getViewer().followObject(mercury, [-0.75, -0.75, 0.5]);
-			break;
-		case "Venus":
-			viz.getViewer().followObject(venus, [-0.75, -0.75, 0.5]);
-			break;
-		case "Earth":
-			viz.getViewer().followObject(earth, [-0.75, -0.75, 0.5]);
-			break;
-		case "Mars":
-			viz.getViewer().followObject(mars, [-0.75, -0.75, 0.5]);
-			break;
-		case "Jupiter":
-			viz.getViewer().followObject(jupiter, [-0.75, -0.75, 0.5]);
-			break;
-		case "Saturn":
-			viz.getViewer().followObject(saturn, [-0.75, -0.75, 0.5]);
-			break;
-		case "Neptune":
-			viz.getViewer().followObject(neptune, [-0.75, -0.75, 0.5]);
-			break;
-		case "Uranus":
-			viz.getViewer().followObject(uranus, [-0.25, -0.25, 0.5]);
-	}
+	//console.log(choiceStr);
+	viz.getViewer().followObject(visualizer_list[choiceStr] , [-0.75 , -0.75 , -0.75]);
 	viz.getViewer().get3jsCamera().zoom = 10;
 	viz.getViewer().get3jsCamera().updateProjectionMatrix();
 });
+
+function appendCheckboxElement(parent_element , child_element_name){
+	var id = child_element_name.concat("-checkbox");
+	var element = document.createElement("input");
+	var element_label = document.createElement("label");
+	element.setAttribute("type" , "checkbox");
+	element.setAttribute("id" , id);
+	element_label.setAttribute("for" , id);
+	element_label.appendChild(element);
+	element_label.append(child_element_name);
+	parent_element.appendChild(element_label);
+	element.checked = true;
+}
+
+function showCheckboxes() {
+	var checkboxes = document.getElementById("checkboxes");
+	if (!expanded) {
+		checkboxes.style.display = "block";
+		expanded = true;
+	} else {
+		checkboxes.style.display = "none";
+		expanded = false;
+  	}
+}
 
 function onDocumentMouseDown(event) {
 	console.log(event);
@@ -150,5 +84,5 @@ function onDocumentMouseDown(event) {
 }
 
 document.querySelectorAll('.vis-controls__set-date').forEach(
-	function(elt){elt.onclick=function(){viz.setDate(
-		new Date(prompt('Enter a date in the format YYYY-mm-dd.','2000-01-01')));};});
+       function(elt){elt.onclick=function(){viz.setDate(
+               new Date(prompt('Enter a date in the format YYYY-mm-dd.','2000-01-01')));};});
