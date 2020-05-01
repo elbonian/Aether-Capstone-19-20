8921804896430fb2b560a0386add3dea60537ace
Yifeng16 <yihu3945@colorado.edu>
2020-05-01 14:17:17 -0600

    Fix a bug caused by multiple time ranges of a single set of bodies.

--- backend/SPKParser.py
+++ backend/SPKParser.py
@@ -64,20 +64,31 @@ class SPKParser:
             #Get Start time and End time of the Kernel   
                 
             elif 'Start of Interval' in lines[i]:
-                line = lines[i+2].strip()
                 
-                #Transfer time formate
-                # time_start = datetime.strptime(line[:24], "%Y %b %d %H:%M:%S.%f").strftime("%Y-%m-%d %H:%M:%S.%f")
-                # time_end = datetime.strptime(line[-24:], "%Y %b %d %H:%M:%S.%f").strftime("%Y-%m-%d %H:%M:%S.%f")
-                # results.append({'bodies': bodies, 'time_start':time_start[:-3], 'time_end':time_end[:-3]})
+                index = 0
                 
-                                
-                # B.C. Check
-                time_start = line[:24] if not 'B.C.' in line[:30] else '0001 JAN 01 00:00:00.000'                    
-                time_end = line[-24:] if 'B.C.' not in line[-30:] else '0001 Dec 31 00:00:00.000'
+                # While one set of bodies have multiple time range:
+                while lines[i+2+index].strip() != '':
+                    
+                    line=lines[i+2+index].strip()
                 
-                results.append({ 'bodies': bodies, 'time_start': time_start, 'time_end': time_end })
-    
+                
+                    #Transfer time formate
+                    # time_start = datetime.strptime(line[:24], "%Y %b %d %H:%M:%S.%f").strftime("%Y-%m-%d %H:%M:%S.%f")
+                    # time_end = datetime.strptime(line[-24:], "%Y %b %d %H:%M:%S.%f").strftime("%Y-%m-%d %H:%M:%S.%f")
+                    # results.append({'bodies': bodies, 'time_start':time_start[:-3], 'time_end':time_end[:-3]})
+
+
+                    # B.C. Check
+                    time_start = line[:24] if 'B.C.' not in line[:30] else '0001 JAN 01 00:00:00.000'                    
+                    time_end = line[-24:] if 'B.C.' not in line[-30:] else '0001 Dec 31 00:00:00.000'
+
+
+
+                    results.append({ 'bodies': bodies, 'time_start': time_start, 'time_end': time_end })
+                    
+                    index += 1
+                    
                 bodies = []
 
         return results
