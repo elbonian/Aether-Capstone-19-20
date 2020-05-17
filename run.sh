b816a9cdcb6a2f75c32ae13948ba03d63c6e8794
Matthew Menten <matthew.menten@colorado.edu>
2020-05-17 16:46:49 -0600

    Minor changes to run script, added more to README

--- run.sh
+++ run.sh
@@ -5,22 +5,18 @@
 function trap_ctrlc ()
 {
 
+    echo
     echo "Ctrl-C caught...performing clean up"
 
-    echo "Stopping backend..."
-    sudo docker stop Aether-Backend
-    sleep 1
-
-    echo "Stopping frontend..."
-    # kill -INT "$NODE_PID"
-    if ps -p "$NODE_PID" > /dev/null
-	then
-   		echo "$NODE_PID -- node server is running"
+    sudo docker stop Aether-Backend > /dev/null
+    echo "Backend docker container stopped."
+    
+    if ps -p "$NODE_PID" > /dev/null; then
    		kill -INT "$NODE_PID"
-   		echo "$NODE_PID -- node server stopped"
+   		echo "Node server stopped"
    	else
-   		echo "$NODE_PID -- node server already stopped."
-	fi
+   		echo "Node server stopped."
+    fi
 
     exit 0
 
@@ -31,14 +27,14 @@ function trap_ctrlc ()
 trap "trap_ctrlc" 2
 
 echo "Starting backend..."
-sudo docker start Aether-Backend
+sudo docker start Aether-Backend > /dev/null
 sleep 1
 
 echo "Starting frontend..."
 node frontend/server.js > /dev/null &
 NODE_PID=$!
 
-while :
-do
-	read -rsp $'Press Ctrl+C to exit...' -n1 key
-done
+echo "Aether is running, connect to localhost:8080 from your web browser."
+
+
+read -rsp $'Press Ctrl+C to exit...' -n1 key
